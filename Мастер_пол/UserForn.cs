91ac2d39a15eb38febcd21c4e686569d756b98d9
dianak27112005 <dianak27112005@gmail.com>
2025-02-10 21:53:51 +0300

    10.02.2025 21.53

---
+++ Мастер_пол/UserForn.cs
@@ -0,0 +1,114 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Мастер_пол
+{
+    public partial class UserForn : Form
+    {
+        public UserForn()
+        {
+            InitializeComponent();
+        }
+
+        private void сотрудникиToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form2 form2 = new Form2();
+            form2.Show();
+            
+        }
+
+        private void ковролинToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            using (var connection = new NpgsqlConnection("Host=localhost; Port=5433; Username=postgres; Password=qwerty; Database=Мастер_пол"))
+            {
+                try
+                {
+                    connection.Open();
+                    string query = "SELECT * FROM Продукция WHERE Тип = 'Ковролин'";
+                    using (var command = new NpgsqlCommand(query, connection))
+                    {
+                        using (var reader = command.ExecuteReader())
+                        {
+                            DataTable dt = new DataTable();
+                            dt.Load(reader);
+
+
+                            dataGridView1.DataSource = dt;
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Ошибка при загрузке данных: " + ex.Message);
+                }
+            }
+        }
+
+        private void паркетнаяДоскаToolStripMenuItem_Click_1(object sender, EventArgs e)
+        {
+            using (var connection = new NpgsqlConnection("Host=localhost; Port=5433; Username=postgres; Password=qwerty; Database=Мастер_пол"))
+            {
+                try
+                {
+                    connection.Open();
+                    string query = "SELECT * FROM Продукция WHERE Тип = 'Паркет'";
+                    using (var command = new NpgsqlCommand(query, connection))
+                    {
+                        using (var reader = command.ExecuteReader())
+                        {
+                            DataTable dt = new DataTable();
+                            dt.Load(reader);
+
+
+                            dataGridView1.DataSource = dt;
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Ошибка при загрузке данных: " + ex.Message);
+                }
+            }
+        }
+
+        private void ламинатToolStripMenuItem_Click_1(object sender, EventArgs e)
+        {
+            using (var connection = new NpgsqlConnection("Host=localhost; Port=5433; Username=postgres; Password=qwerty; Database=Мастер_пол"))
+            {
+                try
+                {
+                    connection.Open();
+                    string query = "SELECT * FROM Продукция WHERE Тип = 'Ламинат'";
+                    using (var command = new NpgsqlCommand(query, connection))
+                    {
+                        using (var reader = command.ExecuteReader())
+                        {
+                            DataTable dt = new DataTable();
+                            dt.Load(reader);
+
+
+                            dataGridView1.DataSource = dt;
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Ошибка при загрузке данных: " + ex.Message);
+                }
+            }
+        }
+
+        private void каталогToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
